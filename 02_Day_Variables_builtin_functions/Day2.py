ec2edd2880f2edc9bc0a9247740cd29a48437cc2
ByteBlitzer <securelycrazy@gmail.com>
2023-08-15 16:20:13 +1000

    Day 2 Exercises

---
+++ 02_Day_Variables_builtin_functions/Day2.py
@@ -0,0 +1,35 @@
+from math import pi
+area_of_circle = float(input("Enter Radius : "))
+print('The area of the circle is ' + str(area_of_circle) + ' is ' + str(pi * area_of_circle**2))
+#The area of the circle is 30.0 is 2827.4333882308138
+# Day 2:30 of python programming
+first_name = 'Cameron'
+last_name = 'Garbo'
+full_name = first_name + last_name
+country = 'Australia'
+age = 28
+year = 2023
+is_married = False
+is_true = True
+is_light_on = 'Yes'
+num_one = 5
+num_two = 4
+
+variable_total = num_one + num_two
+diff = num_one - num_two
+product = num_two * num_one
+division = num_one / num_two
+remainder = num_two % num_one
+exp = num_one ** num_two
+floor_division = num_one // num_two
+
+
+#print(int(floor_division))
+#print(type(last_name))
+#print(type(full_name))
+#print(type(country))
+#print(type(age))
+#print(type(year))
+#print(type(is_married))
+#print(type(is_true))
+#print(type(is_light_on))
